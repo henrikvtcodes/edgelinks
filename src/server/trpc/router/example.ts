b5515d79bef349a3d0b130cbdfc508cf9bd09d92
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-06 09:15:53 -0500

    fixy fixy

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -10,7 +10,5 @@ export const exampleRouter = router({
         greeting: `Hello ${input?.text ?? "world"}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  
 });
