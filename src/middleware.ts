b3a1e773d72b516b451e262dd14f1ed8ad2379ee
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-07 20:03:58 -0500

    remove logging sadge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { PlausibleTracker } from "utils/plausible";
 
 import type { ShortlinkResponse } from "./pages/api/getShortlink/[slug]";
 export const matcher = ["/((?!api|_next/static|favicon.ico).*)", "/:slug"];
-
-// Custom plausible event tracker
-const plausible = new PlausibleTracker(
-  "https://plausible.henriktech.com",
-  "hvt.ski"
-);
 
 export default async function middleware(req: NextRequest) {
   const slug = req.nextUrl.pathname.split("/").pop();
@@ -29,11 +22,6 @@ export default async function middleware(req: NextRequest) {
   const cacheHeader = response.headers.get("cache-control");
   const vercelCacheHeader = response.headers.get("x-vercel-cache");
 
-  // const allHeaders: { [key: string]: string } = {};
-  // response.headers.forEach((value, key) => {
-  //   allHeaders[key] = value;
-  // });
-
   const data: ShortlinkResponse = await response.json();
   console.log(
     JSON.stringify(
@@ -42,30 +30,11 @@ export default async function middleware(req: NextRequest) {
         fetchTime: `${endFetch - startFetch}ms`,
         cacheHeader,
         vercelCacheHeader,
-        // headers: allHeaders,
       },
       null,
       4
     )
   );
 
-  // Track pageview
-  await plausible.sendPageview({
-    url: req.referrer,
-    userAgent: req.headers.get("user-agent") ?? "",
-    xForwardedFor: req.headers.get("x-forwarded-for") ?? "",
-    name: "pageview",
-  });
-
-  // Track outbound link
-  await plausible.sendEvent({
-    url: req.referrer,
-    userAgent: req.headers.get("user-agent") ?? "",
-    xForwardedFor: req.headers.get("x-forwarded-for") ?? "",
-    name: "Outbound Link",
-    props: {
-      url: data.url,
-    },
-  });
   return NextResponse.redirect(data.url);
 }
